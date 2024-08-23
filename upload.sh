4eb84681dea5068819730980666ae10a8843dd48
Fredrik Vold <fredrik@webkonsept.com>
2024-08-23 16:55:50 +0200

    Upload script reports actual upload error
    Until I get real tests, at least I should print the error!

--- upload.sh
+++ upload.sh
@@ -8,6 +8,10 @@ PILE=84ed8bd7-f8a1-4e4b-bc4d-85868208dae5
 TOKEN=d5e1c56e-c8db-4b2a-adbe-60167ddee431
 DATA=$(/usr/bin/env curl -s -X POST --oauth2-bearer "$TOKEN" -H "Content-Type: multipart/form-data" -F "data=@$FILE" http://localhost:1995/$PILE/)
 ENTRY=$(jq -r '.entry' <<< "$DATA")
+if [ "$ENTRY" == "null" ]; then
+    echo $DATA
+    exit 1
+fi
 echo http://localhost:1995/$PILE/$ENTRY
 /usr/bin/env curl -s --oauth2-bearer "$TOKEN" http://localhost:1995/$PILE/$ENTRY
 
